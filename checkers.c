7eecbaade8a075d27108ede4d15f838ab24983ce
RafaelCopes <rafaelcopes16@gmail.com>
2023-07-23 12:33:29 -0300

    add logic for king piece

--- checkers.c
+++ checkers.c
@@ -14,6 +14,17 @@ void makeMove(int turn, int fromRow, int fromCol, int toRow, int toCol);
 int isGameOver();
 int getPlayerMove(int turn, int* fromRow, int* fromCol, int* toRow, int* toCol);
 
+/*int board[BOARD_SIZE][BOARD_SIZE] = {
+	{2, 0, 2, 0, 2, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{2, 0, 2, 0, 0, 0, 0, 0},
+	{0, 0, 0, 3, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{1, 0, 1, 0, 1, 0, 0, 0},
+	{0, 1, 0, 1, 0, 4, 0, 3},
+};*/
+
 int board[BOARD_SIZE][BOARD_SIZE];
 int turn = PLAYER1;
 
@@ -128,7 +139,7 @@ int isNotWithinBounds(int toRow, int toCol) {
 }
 
 // check if a move is valid
-int isValidMove(int player, int fromRow, int fromCol, int toRow, int toCol) {
+int isValidMove(int turn, int fromRow, int fromCol, int toRow, int toCol) {
 	// check if the destination is within the bounds of the board
 	if (isNotWithinBounds(toRow, toCol)) return 0;
 
@@ -136,56 +147,84 @@ int isValidMove(int player, int fromRow, int fromCol, int toRow, int toCol) {
 	if (board[toRow][toCol] != EMPTY_CELL) return 0;
 
 	// check if the piece is moving diagonally
-	//if (fromRow == toRow || fromCol == toCol) return 0;
-
-	// check if the piece is moving diagonally
-	//int rowDiff = abs(toRow - fromRow);
-	//int colDiff = abs(toCol - fromCol);
-	//if (rowDiff != colDiff) return 0;
-
-	// check if the piece is moving diagonally
 	int rowDiff = abs(toRow - fromRow);
 	int colDiff = abs(toCol - fromCol);
 	if (rowDiff == 0 || colDiff == 0 || rowDiff != colDiff) return 0;
 
+	// check if is a king move
+	int isKing = (board[fromRow][fromCol] == turn + 2) ? 1 : 0;
+
 	// check if the piece is moving in the correct direction
-	int direction = (player == PLAYER1) ? -1 : 1;
+	int direction = (turn == PLAYER1) ? -1 : 1;
 	int rowMove = toRow - fromRow;
-	if (rowMove * direction < 0) return 0;
+	if (rowMove * direction < 0 && !isKing) return 0;
 
 	// check the distance of the move to see if is regular to capture move
 	int distance = abs(toCol - fromCol) > abs(toRow - fromRow) ? abs(toCol - fromCol) : abs(toRow - fromRow);
-	if (distance != 1 && distance != 2) return 0;
+	if (distance != 1 && distance != 2 && !isKing) return 0;
 
 	// if it is a capture move, check if the opponent piece is in the middle
-	if (distance == 2) {
+	if (distance == 2 && !isKing) {
 		int midRow = (fromRow + toRow) / 2;
 		int midCol = (fromCol + toCol) / 2;
 
-		if (board[midRow][midCol] != (player % 2) + 1) return 0;
+		// check if the middle cell contains an opponent's piece or opponent king
+		if (board[midRow][midCol] == turn || board[midRow][midCol] == turn + 2) return 0;
+	}
+
+	// check for conditions if it is a king piece
+	if (isKing) {
+		int rowDiff = abs(toRow - fromRow);
+		int colDiff = abs(toCol - fromCol);
+		if (rowDiff != colDiff) return 0;
+
+		// check if the path is clear for a king move
+		int i, row, col;
+		int rowDir = (toRow - fromRow) / rowDiff; // 1 or -1
+		int colDir = (toCol - fromCol) / colDiff; // 1 or -1
+		for (i = 1; i < rowDiff - 1; i++) {
+			row = fromRow + i * rowDir;
+			col = fromCol + i * colDir;
+			if (board[row][col] != EMPTY_CELL) return 0;
+		}
 	}
 
 	return 1; // move is valid
 }
-
+ 
 // function to update the board after a valid move
 void makeMove(int turn, int fromRow, int fromCol, int toRow, int toCol) {
+	int isKing = (board[fromRow][fromCol] == turn + 2) ? 1 : 0;
+	
 	// move the piece to the destination cell
 	board[toRow][toCol] = board[fromRow][fromCol];
 	board[fromRow][fromCol] = EMPTY_CELL;
 
 	// check if the piece should be promoted to a king
-	if ((turn == PLAYER1 && toRow == 0) ||
-		(turn == PLAYER2 && toRow == BOARD_SIZE - 1)) {
-		board[toRow][toCol] += 2; // promote the piece to a king
+	if ((turn == PLAYER1 && toRow == 0) || (turn == PLAYER2 && toRow == BOARD_SIZE - 1)) {
+		// check if the piece is not already a king before promoting it
+
+		if (board[toRow][toCol] == PLAYER1 || board[toRow][toCol] == PLAYER2) {
+			board[toRow][toCol] += 2; // promote the piece to a king
+		}
 	}
 
-	// maybe change this ???????????????????
 	// if it is a capture move, remove the captured piece
-	if (abs(toRow - fromRow) == 2) {
+	if (abs(toRow - fromRow) == 2 && !isKing) {
 		int midRow = (fromRow + toRow) / 2;
 		int midCol = (fromCol + toCol) / 2;
 		board[midRow][midCol] = EMPTY_CELL;
+	}
+
+	// capture move for a king piece
+	if (isKing) {
+		int rowDiff = abs(toRow - fromRow);
+		int colDiff = abs(toCol - fromCol);
+
+		// check if the path is clear for a king move
+		int rowDir = (toRow - fromRow) / rowDiff; // 1 or -1
+		int colDir = (toCol - fromCol) / colDiff; // 1 or -1
+		board[toRow-rowDir][toCol-colDir] = EMPTY_CELL;
 	}
 }
 
@@ -202,16 +241,17 @@ int isGameOver() {
 			}
 	}
 
-	if (player1Pieces == 0 || player2Pieces == 0)
-		return 1;
+	if (player1Pieces == 0 || player2Pieces == 0) return 1;
+
+	// stalemate causing errors, maybe remove it ?????????
 
 	// check for a stalemate (no more possible moves)
-	int currentPlayerPieces = (player1Pieces > player2Pieces) ? PLAYER1 : PLAYER2;
+	/*int currentPlayerPieces = (player1Pieces > player2Pieces) ? PLAYER1 : PLAYER2;
 	int stalemate = 1;
 	for (int row = 0; row < BOARD_SIZE; ++row) {
 		for (int col = 0; col < BOARD_SIZE; ++col) {
 			if (board[row][col] == currentPlayerPieces) {
-				// Check if the current player has any valid moves
+				// check if the current player has any valid moves
 				if ((row > 0 && col > 0 && isValidMove(currentPlayerPieces, row, col, row - 1, col - 1)) ||
 					(row > 0 && col < BOARD_SIZE - 1 && isValidMove(currentPlayerPieces, row, col, row - 1, col + 1)) ||
 					(row < BOARD_SIZE - 1 && col > 0 && isValidMove(currentPlayerPieces, row, col, row + 1, col - 1)) ||
@@ -227,8 +267,10 @@ int isGameOver() {
 			break;
 	}
 
-	if (stalemate)
+	if (stalemate) {
+		printf("stalemateeeeee");
 		return 1;
+	}*/
 
 	// maybe add more conditions ????????
 
